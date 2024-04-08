57d5a7d6823d2a6bec8c4e4dad8350f4a304026b
VidhiShah195 <shahvidhi195@gmail.com>
2024-04-08 19:42:59 -0400

    Added another Bar Plot

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ stops = pd.read_csv("data/Officer_Traffic_Stops.csv")
 
 st.dataframe(stops)
 
-# Vidhi Shah Was Here :)
+# Vidhi Shah Was Here :) 
+
+## Box plot
+alt.Chart(stops).mark_boxplot().encode(
+    x = alt.X('Was_a_Search_Conducted'),
+    y = alt.Y('Driver_Age')
+).properties(
+    width = 500,
+    title = 'Boxplot between Search Conducted vs Driver Age')
