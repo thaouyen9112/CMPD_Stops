82a3c5bcb466576752234edebde07bf7efa0f735
VidhiShah195 <shahvidhi195@gmail.com>
2024-04-08 19:52:51 -0400

    I added a boxplot

---
+++ pages/2_boxplot.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+@st.cache_data  # 👈 Add the caching decorator
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+
+stops = pd.read_csv("data/Officer_Traffic_Stops.csv")
+
+## Box plot
+box=alt.Chart(stops).mark_boxplot().encode(
+    x = alt.X('Was_a_Search_Conducted'),
+    y = alt.Y('Driver_Age')
+).properties(
+    width = 500,
+    title = 'Boxplot between Search Conducted vs Driver Age')
+
+st.altair_chart(stops)
